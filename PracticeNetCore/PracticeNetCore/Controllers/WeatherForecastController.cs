849a288268b8dd9eb4964ecdc26e6cb41fd77cab
Nguyễn Văn Liêm <62871767+liemnv60cntt@users.noreply.github.com>
2022-11-18 10:55:10 +0700

    Update WeatherForecastController.cs

--- PracticeNetCore/PracticeNetCore/Controllers/WeatherForecastController.cs
+++ PracticeNetCore/PracticeNetCore/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace PracticeNetCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Scorching", "Scorching", "Scorching", "Scorching", "Scorching", "Scorching", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
