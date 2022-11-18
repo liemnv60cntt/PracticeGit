8d25dba0727faea9c6caafe54bd6238daeee74c3
Liem Van Nguyen <nvliemc4ncthanh@gmail.com>
2022-11-18 10:45:07 +0700

    Add get method

--- PracticeNetCore/PracticeNetCore/Controllers/WeatherForecastController.cs
+++ PracticeNetCore/PracticeNetCore/Controllers/WeatherForecastController.cs
@@ -29,5 +29,16 @@ namespace PracticeNetCore.Controllers
             })
             .ToArray();
         }
+        [HttpGet(Name = "GetData")]
+        public IEnumerable<WeatherForecast> GetData()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
